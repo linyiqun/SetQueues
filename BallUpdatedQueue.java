8a747c4e1afe1ecdff8072a3cfa997b65d7ecf6e
lyq <675250079@qq.com>
2016-12-06 19:33:00 +0800

    Update BallUpdatedQueue.java

--- BallUpdatedQueue.java
+++ BallUpdatedQueue.java
@@ -1,20 +1,20 @@
 public class BallUpdatedQueue extends UpdatedQueue<String, Ball> {
 	@Override
-	public boolean offer(Ball newRow) {
-		Ball row = null;
+	public boolean offer(Ball newBall) {
+		Ball ball = null;
 		// 获取对象的唯一键值
-		String uniqueKey = newRow.getUniqueKey();
+		String uniqueKey = newBall.getUniqueKey();
 
 		lock.lock();
 		try {
 			// 如果当前图中已经包含此键值，表明队列中已有同属性对象
 			if (map.containsKey(uniqueKey)) {
 				// 取出此键值对应的对象，进行属性值的更新，因为map中的对象与queue中的对象是同一引用，所以能达到更新的效果
-				row = map.get(uniqueKey);
-				row.valueUpdated(newRow);
+				ball = map.get(uniqueKey);
+				ball.valueUpdated(newBall);
 			} else {
-				map.put(uniqueKey, newRow);
-				queue.offer(newRow);
+				map.put(uniqueKey, newBall);
+				queue.offer(newBall);
 			}
 		} finally {
 			lock.unlock();
@@ -25,17 +25,17 @@ public class BallUpdatedQueue extends UpdatedQueue<String, Ball> {
 
 	@Override
 	public Ball poll() {
-		Ball row = null;
+		Ball ball = null;
 		String uniqueKey;
 
 		lock.lock();
 		try {
 			// 取出队列头部对象
-			row = queue.poll();
+			ball = queue.poll();
 
-			if (row != null) {
+			if (ball != null) {
 				// 取出对象的唯一键值
-				uniqueKey = row.getUniqueKey();
+				uniqueKey = ball.getUniqueKey();
 				// 从图中也同样移除
 				map.remove(uniqueKey);
 			}
@@ -43,6 +43,6 @@ public class BallUpdatedQueue extends UpdatedQueue<String, Ball> {
 			lock.unlock();
 		}
 
-		return row;
+		return ball;
 	}
 }
